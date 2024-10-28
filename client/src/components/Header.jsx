e6adbc7e45906734274a901ebc99ebbb235dedf3
bradleyyang <bradley.yang023@gmail.com>
2024-10-28 00:05:48 -0400

    edited navbar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return (
+        <nav style={{height: '10vh', backgroundColor: 'rgb(255, 248, 220)'}}>
+            <ul className="nav-list">
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Experience
+                </li>
+                <li>
+                    Contact
+                </li>
+                <li>
+                    Projects
+                </li>
+            </ul>
+        </nav>
+    )
+}
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,9 @@
+import Header from "./Header";
+
 const Layout = () => {
     return (
         <div style={{ color: 'black', backgroundColor: 'white', height: '100vh', width: '100vw' }}>
-            
+            <Header />
         </div>
     );
 }
