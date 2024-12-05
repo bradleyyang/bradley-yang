a105f76c4444dd393509848b6c667f45079e93ae
bradleyyang <bradley.yang023@gmail.com>
2024-12-05 00:25:58 -0500

    added skeleton of contact form in contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Contact = () => {
     const navigate = useNavigate();
+    const [name, setName] = useState("");
     return (
         <div>
             <nav className="navbar">
@@ -37,8 +39,16 @@ const Contact = () => {
                 </div>
             </nav>
             <hr style={{ marginTop: "15px", marginBottom: "15px" }} />
-            <div className="contact-page">
-                Contact form here
+            <div style={{width: "100%"}}>
+                <form style={{display: "flex", flexDirection: "column", width: "80%", justifyContent: "center"}}>
+                    <input placeholder="Email" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input placeholder="Message" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+                    <button style={{height: "20px", width: "200px"}}>
+                        Submit
+                    </button>
+                </form>
             </div>
         </div>
     );
